86e0b81f869ad1cd37a7b170a95a6eae97960142
Elena Grahovac <elena@grahovac.me>
2017-02-05 16:23:04 +0700

    Add basic source code

---
+++ ex-01-server/server.go
@@ -0,0 +1,18 @@
+// Example from "The Go Programming Language" book: https://github.com/adonovan/gopl.io/blob/master/ch1/server1/main.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+// handler echoes the Path component of the requested URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
